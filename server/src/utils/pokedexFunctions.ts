5faaf8982e6e745e6e0e87c7ed8ebc62d3633f47
freddan88 <fredrik@leemann.se>
2022-09-01 19:47:56 +0200

    Clenup code even more

--- server/src/utils/pokedexFunctions.ts
+++ server/src/utils/pokedexFunctions.ts
@@ -2,6 +2,11 @@ import axios from "axios";
 import { API_POKEMON_LIMIT } from "./pokedexConstants";
 import { ICharactersData, TCharacters } from "./pokedexTypes";
 
+const getImage = (imgUrlA: string, imgUrlB: string) => {
+  if (imgUrlA) return imgUrlA;
+  return imgUrlB;
+};
+
 export const capitalizeFirst = (name: string) => {
   const firstCharacter = name.slice(0, 1);
   const restCharacters = name.slice(1, name.length);
@@ -17,16 +22,14 @@ export const getCharacterInfo = async (
   const promises = results.map((obj) => axios.get<ICharactersData>(obj.url));
   const resolved = await Promise.all(promises);
   const characters = resolved.map((apiRes) => {
-    const { other, front_default } = apiRes.data.sprites;
-    const image = other.dream_world.front_default
-      ? other.dream_world.front_default
-      : front_default;
-    const { id, name } = apiRes.data;
+    const { id, name, sprites, types } = apiRes.data;
+    const { other, front_default } = sprites;
+    const image = getImage(other.dream_world.front_default, front_default);
     const modifiedName = capitalizeFirst(name);
     return {
       id,
       name: modifiedName,
-      type: apiRes.data.types[0].type.name,
+      type: types[0].type.name,
       sprite: image,
     };
   });
