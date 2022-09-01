47188816f6aff222e3bc41c4dc25c44238622302
freddan88 <fredrik@leemann.se>
2022-09-01 21:30:24 +0200

    Add more routes to user-routes

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,6 +1,31 @@
 import { Request as Req, Response as Res } from "express";
 import { users } from "../data/users";
+import { TUserParams } from "../routes/userRoutes";
 
 export const index = (req: Req, res: Res) => {
+  res.status(200).json(users);
+};
+
+export const create = (req: Req, res: Res) => {
+  res.status(200).json(users);
+};
+
+export const store = (req: Req, res: Res) => {
+  res.status(200).json(users);
+};
+
+export const show = (req: Req<TUserParams>, res: Res) => {
+  res.status(200).json(users);
+};
+
+export const edit = (req: Req<TUserParams>, res: Res) => {
+  res.status(200).json(users);
+};
+
+export const update = (req: Req<TUserParams>, res: Res) => {
+  res.status(200).json(users);
+};
+
+export const destroy = (req: Req<TUserParams>, res: Res) => {
   res.status(200).json(users);
 };
--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,8 +1,20 @@
 import { Router } from "express";
 import * as userController from "../controllers/userController";
 
+export type TUserParams = {
+  id: string;
+};
+
 const userRoutes = Router();
 
 userRoutes.get("/", userController.index);
+
+userRoutes.get("/:id", userController.show);
+
+userRoutes.post("/", userController.store);
+
+userRoutes.put("/:id", userController.update);
+
+userRoutes.delete("/:id", userController.destroy);
 
 export default userRoutes;
