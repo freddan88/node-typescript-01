5c9f4c29fd47aaa1d69f130714a3f6359c51f086
freddan88 <fredrik@leemann.se>
2022-09-01 21:32:44 +0200

    Add id:s to user-data

--- server/src/data/users.ts
+++ server/src/data/users.ts
@@ -1,4 +1,5 @@
 export type TUsers = {
+  id: number;
   name: string;
   phone: string;
   email: string;
@@ -7,30 +8,35 @@ export type TUsers = {
 
 export const users: TUsers[] = [
   {
+    id: 1,
     name: "Brooke Maxwell",
     phone: "1-234-718-0389",
     email: "laoreet.libero@yahoo.org",
     country: "Vietnam",
   },
   {
+    id: 2,
     name: "Elliott William",
     phone: "1-392-365-2241",
     email: "mattis.velit@protonmail.couk",
     country: "Russian Federation",
   },
   {
+    id: 3,
     name: "Elmo Mckay",
     phone: "(457) 463-6150",
     email: "non.cursus.non@yahoo.com",
     country: "Germany",
   },
   {
+    id: 4,
     name: "Germane Barry",
     phone: "(544) 477-4628",
     email: "malesuada@aol.org",
     country: "Norway",
   },
   {
+    id: 5,
     name: "Erin Clemons",
     phone: "(773) 595-9494",
     email: "magna.malesuada.vel@aol.ca",
