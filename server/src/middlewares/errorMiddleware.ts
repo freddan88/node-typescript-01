2f547ddc0ef00ada97bf0c2e2a95d16dc5d9c54c
freddan88 <fredrik@leemann.se>
2022-09-03 02:20:17 +0200

    Updated comments

--- server/src/middlewares/errorMiddleware.ts
+++ server/src/middlewares/errorMiddleware.ts
@@ -1,8 +1,9 @@
 import { Request as Req, Response as Res, NextFunction as Next } from "express";
 
 const errorMiddleware = (err: Error, req: Req, res: Res, next: Next) => {
-  // console.log(res.statusCode);
   // console.log(res.statusMessage);
+  // console.log(res.statusCode);
+  // console.log(next.caller);
   console.error(err);
   res.status(res.statusCode || 500);
   res.json({
