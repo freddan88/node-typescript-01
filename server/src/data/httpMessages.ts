3ab84a367ba9b5344427a7c5a190244ec5f641ed
freddan88 <fredrik@leemann.se>
2022-09-10 13:59:16 +0200

    Remove unused file and update httpCodes

--- server/src/data/httpMessages.ts
+++ server/src/data/httpMessages.ts
@@ -5,8 +5,24 @@ type TStatuses = {
 };
 
 const httpMessages: TStatuses = {
+  // https://httpstatuses.org
+  100: { httpMessage: "Continue" },
+  102: { httpMessage: "Processing" },
+  200: { httpMessage: "OK" },
+  201: { httpMessage: "Created" },
+  202: { httpMessage: "Accepted" },
+  204: { httpMessage: "No Content" },
   400: { httpMessage: "Bad Request" },
+  401: { httpMessage: "Unauthorized" },
+  403: { httpMessage: "Forbidden" },
+  404: { httpMessage: "Not Found" },
+  405: { httpMessage: "Method Not Allowed" },
+  408: { httpMessage: "Request Timeout" },
+  418: { httpMessage: "I'm a teapot" },
+  429: { httpMessage: "Too Many Requests" },
   500: { httpMessage: "Internal Server Error" },
+  501: { httpMessage: "Not Implemented" },
+  503: { httpMessage: "Service Unavailable" },
 };
 
 export const positiveParam = {
